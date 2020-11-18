17b85884c92c24dc51770d0090d7d14eb6f648c0
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2020-11-18 20:59:19 +0200

    changed position of the windows

--- runPings.sh
+++ runPings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 open bezeq_google.command
-osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {100, 100, 320, 300}'
+osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {440, 750, 660, 950}'
 open bezeq_il.command
-osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {320, 100, 540, 300}'
+osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {660, 750, 880, 950}'
 open hot_google.command
-osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {540, 100, 760, 300}'
+osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {880, 750, 1100, 950}'
 open hot_il.command
-osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {760, 100, 980, 300}'
+osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {1100, 750, 1320, 950}'
 open hot_wifi.command
-osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {980, 100, 1200, 300}'
+osascript -e 'tell application "Terminal" to tell window 1 to set bounds to {1320, 750, 1540, 950}'
